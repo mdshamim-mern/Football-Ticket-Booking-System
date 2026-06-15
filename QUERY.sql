c7288be866f53d3f92c61893f87159b1c88093d7
Md Shamim <mdshamim.mern@gmail.com>
2026-06-15 13:29:25 +0600

    Solved Query 3 handling NULL values using COALESCE

--- QUERY.sql
+++ QUERY.sql
@@ -85,3 +85,8 @@ WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 SELECT user_id, full_name, email 
 FROM Users 
 WHERE full_name LIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+-- Query 3:
+SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status 
+FROM Bookings 
+WHERE payment_status IS NULL;
