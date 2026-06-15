47558c62fc84dda92f932b386501be67423aa9c6
Md Shamim <mdshamim.mern@gmail.com>
2026-06-15 13:30:49 +0600

    Solved Query 5 implementing LEFT JOIN to include users without bookings

--- QUERY.sql
+++ QUERY.sql
@@ -99,3 +99,9 @@ SELECT b.booking_id, u.full_name, m.fixture, b.total_cost
 FROM Bookings b 
 INNER JOIN Users u ON b.user_id = u.user_id 
 INNER JOIN Matches m ON b.match_id = m.match_id;
+
+
+-- Query 5:
+SELECT u.user_id, u.full_name, b.booking_id 
+FROM Users u 
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
